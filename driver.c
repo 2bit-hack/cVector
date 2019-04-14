7df9827ccd9215e1791a588d69e954d92edc346d
Soham Kar <sohamkar76@gmail.com>
2019-04-14 19:51:43 +0530

    Update driver.c

--- driver.c
+++ driver.c
@@ -1,24 +1,28 @@
 #include "vector.h"
 
 int main() {
-    Vector* v = vect_createVector();
-    Vector* u = vect_createVectorWithSize(10);
-
+    //Vector* v = vect_createVector();
+    Vector* u = vect_createVectorWithSize(5);
+    DEBUG_printSize(u);
+    DEBUG_printCurrentSize(u);
+    printf("***\n");
     for(int i = 0; i < 10; i++) {
-        //DEBUG_printMemAlloc(u);
         //DEBUG_printMemAlloc(v);
         vect_insert(u, i+1);
-        vect_insert(v, i+1);
+        DEBUG_printSize(u);
+        DEBUG_printCurrentSize(u);
+        printf("%d\n", vect_at(u, i));
+        //vect_insert(v, i+1);
     }
 
-    vect_print(v);
+    //vect_print(v);
     vect_print(u);
     
-    int x = vect_at(v, 8);
-    printf("%d\n", x);
+    //int x = vect_at(v, 8);
+    //printf("%d\n", x);
 
     vect_clear(u);
-    vect_clear(v);
+    //vect_clear(v);
 
     return 0;
 }
